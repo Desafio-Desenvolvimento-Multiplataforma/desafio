bc67380029d3ace4d2814bba57bd4c90c81eb1aa
Bruna Adam <brunaadam54@gmail.com>
2025-05-13 15:16:08 -0300

    Commit Inicial

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+    import Header from './components/Header.vue';
+    import SeletorSetor from './components/Seletores/Seletor-setor.vue';
+    import SeletorLocal from './components/Seletores/Seletor-local.vue';
+    import Mapa from './components/Mapa.vue';
+    import Footer from './components/Footer.vue';
+</script>
+
+<template>
+  <Header></Header>
+  <SeletorSetor></SeletorSetor>
+  <SeletorLocal></SeletorLocal>
+  <Mapa></Mapa>
+  <Footer></Footer>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+
+//Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+//Componente App
+import App from './App.vue'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+const app = createApp(App)
+
+app.use(vuetify).mount('#app')
