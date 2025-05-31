639559a60385ea9c5f2c3d71e1c95df5f535a084
I753118 <tainna.ribeiro@sap.com>
2025-05-31 02:42:01 -0300

    Adição de novos setores A, B, H e I com locais e coordenadas geográficas

--- src/components/Seletores/Setores.ts
+++ src/components/Seletores/Setores.ts
@@ -1,5 +1,130 @@
 export const setoresUnisinos = [
   {
+    name: "Setor A",
+    local: [
+      {
+        name: "A01 Centro Comunitário",
+        latitude: -29.794492439669536,
+        longitude: -51.15533701683996
+      },
+      {
+        name: "A02 Biblioteca Unisinos",
+        latitude: -29.794883427939055,
+        longitude: -51.156506526901445
+      },
+      {
+        name: "A1 Estacionamento",
+        latitude: -29.792892726646897,
+        longitude: -51.156782896864954
+      },
+      {
+        name: "A2 Estacionamento",
+        latitude:  -29.793692127791658,
+        longitude: -51.15635071392417
+      },
+      {
+        name: "A3 Estacionamento",
+        latitude: -29.79507407179408,
+        longitude: -51.15714859212644
+      },
+      {
+        name: "A4 Estacionamento",
+        latitude: -29.795349159538425,
+        longitude: -51.156271722838525
+      }
+    ]
+  },
+  {
+    name: "Setor B",
+    local: [
+      {
+        name: "B01",
+        latitude: -29.792531609117027,
+        longitude: -51.154927339914195
+      },
+      {
+        name: "B02",
+        latitude: -29.792491204420447,
+        longitude: -51.15403528553096
+      },
+      {
+        name: "B03",
+        latitude: -29.792806476728035,
+        longitude: -51.15471368343212
+      },
+      {
+        name: "B04",
+        latitude: -29.79286467223331,
+        longitude: -51.1538768329297
+      },
+      {
+        name: "B05",
+        latitude: -29.793213728620866,
+        longitude: -51.1547835163081
+      },
+      {
+        name: "B06",
+        latitude: -29.79322209297262,
+        longitude: -51.15384282771633
+      },
+      {
+        name: "B07",
+        latitude: -29.79355438317733,
+        longitude: -51.154122482844464
+      },
+      {
+        name: "B08 Auditório Central",
+        latitude: -29.793433345499288,
+        longitude: -51.15363163884169
+      },
+      {
+        name: "B09",
+        latitude: -29.79301114961461,
+        longitude: -51.155519233961755
+      },
+      {
+        name: "B10",
+        latitude: -29.793896761036752,
+        longitude: -51.15353026290199
+      },
+      {
+        name: "B11",
+        latitude: -29.79418304037594,
+        longitude: -51.15338965489627
+      },
+      {
+        name: "B12",
+        latitude: -29.794459933342655,
+        longitude: -51.15323282215113
+      },
+      {
+        name: "B13",
+        latitude: -29.794604507079054,
+        longitude: -51.153225503152825
+      },
+      {
+        name: "B14",
+        latitude: -29.794874722435253,
+        longitude: -51.15311560534193
+      },
+      {
+        name: "B15",
+        latitude: -29.795144936214744,
+        longitude: -51.15306065845029
+      },
+      {
+        name: "B16",
+        latitude: -29.79480886494038,
+        longitude: -51.153696028941674
+      },
+      {
+        name: "B17",
+        latitude: -29.794296938334266,
+        longitude: -51.154165562610174
+      }
+    ]
+  },
+  {
     name: "Setor C",
     local: [
       {
@@ -131,6 +256,36 @@ export const setoresUnisinos = [
         name: "K01",
         latitude: -29.797469584303816,
         longitude: -51.15884868291968
+      }
+    ]
+  },
+  {
+    name: "Setor H",
+    local: [
+      {
+        name: "H01 Complexo Desportivo",
+        latitude: -29.79739667956847,
+        longitude: -51.15220219021463
+      },
+      {
+        name: "H02",
+        latitude: -29.797742751344938,
+        longitude: -51.15215598731072
+      },
+      {
+        name: "H03",
+        latitude: -29.797540042466746,
+        longitude: -51.15312560055706
+      }
+    ]
+  },
+  {
+    name: "Setor I",
+    local: [
+      {
+        name: "Área de futura expansão",
+        latitude: -29.798999858530117,
+        longitude: -51.155586784023605
       }
     ]
   }
